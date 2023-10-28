544b5fc5260d197430aca8bb4a71fe442c544922
David Ukelere <david.ukelere291@gmail.com>
2023-10-28 07:59:02 +0100

    Added get user friends route

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,40 @@
-const getUser = async (req, res) => {};
+import User from "../models/user.js";
 
-const getUserFriends = async (req, res) => {};
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return {
+          _id,
+          firstName,
+          lastName,
+          occupation,
+          location,
+          picturePath,
+        };
+      }
+    );
+    return res.status(200).json(formattedFriends);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
 
 const addRemoveFriend = async (req, res) => {};
 
