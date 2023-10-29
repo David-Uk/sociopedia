6ad4ba2ea857d00f18a9ec90fa0e864b40530ee8
David Ukelere <david.ukelere291@gmail.com>
2023-10-29 17:39:25 +0100

    Added get user posts route

--- controllers/Posts.js
+++ controllers/Posts.js
@@ -34,3 +34,34 @@ export const getFeedPosts = async (req, res) => {
     res.status(404).json({ error: error.message });
   }
 };
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    return res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = posts.like.get(userId);
+
+    if (isLiked) post.likes.delete(userId);
+    post.likes.set(userId, true);
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    return res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
