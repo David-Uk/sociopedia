661122a8b451aad61b1c73a6cc7eb97f03a57841
David Ukelere <david.ukelere291@gmail.com>
2023-10-30 03:32:05 +0100

    Added like posts route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { createPost, getFeedPosts } from "../controllers/Posts.js";
+import {
+  createPost,
+  getFeedPosts,
+  getUserPosts,
+  likePost,
+} from "../controllers/Posts.js";
 import upload from "../utils/upload.js";
 
 const postRoutes = express.Router();
 
 postRoutes.post("/", verifyToken, upload.single("picture"), createPost);
 postRoutes.get("/", verifyToken, getFeedPosts);
+postRoutes.get("/:id", verifyToken, getUserPosts);
+postRoutes.patch("/like/:id", verifyToken, likePost);
 
 export default postRoutes;
